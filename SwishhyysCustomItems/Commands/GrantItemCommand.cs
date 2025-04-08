bdc88fb19eeb9f6da43b7896fc2b8965d3d05790
Swishhyy <72903966+Swishhyy@users.noreply.github.com>
2025-04-08 11:28:32 -0500

    Implemented a grant item command
    this one is just for debugging primarily

---
+++ SwishhyysCustomItems/Commands/GrantItemCommand.cs
@@ -0,0 +1,50 @@
+﻿using CommandSystem;
+using Exiled.API.Features;
+using Exiled.CustomItems.API.Features;
+using Exiled.Permissions.Extensions;
+using SCI.Custom.MedicalItems;
+using System;
+
+namespace SCI.Commands
+{
+    [CommandHandler(typeof(RemoteAdminCommandHandler))]
+    public class GrantItemCommand : ICommand
+    {
+        public string Command { get; } = "giveadrenaline";
+
+        public string[] Aliases { get; } = { "gad" };
+
+        public string Description { get; } = "Grants the Adrenaline Pills item to yourself.";
+
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            var player = Player.Get(sender);
+
+            if (player == null)
+            {
+                response = "This command can only be used by a player.";
+                return false;
+            }
+
+            // Check if the sender has the required permission
+            if (!sender.CheckPermission("sci.giveadrenaline"))
+            {
+                response = "You do not have permission to use this command.";
+                return false;
+            }
+
+            // Give the Adrenaline Pills item
+            if (CustomItem.TryGet(101, out var item))
+            {
+                item.Give(player);
+                response = "You have been given the Adrenaline Pills.";
+                return true;
+            }
+            else
+            {
+                response = "Failed to retrieve the Adrenaline Pills item.";
+                return false;
+            }
+        }
+    }
+}
